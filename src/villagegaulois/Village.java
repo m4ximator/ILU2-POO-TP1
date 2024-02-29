ca4d492b2b80fc51d06585d9bc93d3d7050b3587
Maxime Cherraf <chm3590b@ensfsi.onmicrosoft.com>
2024-02-29 19:58:50 +0100

    seance tp1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -45,15 +45,67 @@ public class Village {
 	public String afficherVillageois() {
 		StringBuilder chaine = new StringBuilder();
 		if (nbVillageois < 1) {
-			chaine.append("Il n'y a encore aucun habitant au village du chef "
-					+ chef.getNom() + ".\n");
+			chaine.append("Il n'y a encore aucun habitant au village du chef " + chef.getNom() + ".\n");
 		} else {
-			chaine.append("Au village du chef " + chef.getNom()
-					+ " vivent les légendaires gaulois :\n");
+			chaine.append("Au village du chef " + chef.getNom() + " vivent les légendaires gaulois :\n");
 			for (int i = 0; i < nbVillageois; i++) {
 				chaine.append("- " + villageois[i].getNom() + "\n");
 			}
 		}
 		return chaine.toString();
+	}
+
+	private static class Marche {
+		private Etal[] etals;
+
+		public Marche(int nbEtal) {
+			etals = new Etal[nbEtal];
+		}
+
+		void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			for (int i = 0; i < etals.length; i++) {
+				if (i == indiceEtal) {
+					etals[i].occuperEtal(vendeur, produit, nbProduit);
+				}
+			}
+		}
+
+		int trouverEtalLibre() {
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].isEtalOccupe() == true) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		Etal[] trouverEtals(String produit) {
+			int compteurEtal = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].contientProduit(produit)) {
+					compteurEtal++;
+				}
+			}
+			Etal[] newEtal = new Etal[compteurEtal];
+			int index = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].contientProduit(produit)) {
+					newEtal[index] = etals[i];
+				}
+			}
+			return newEtal;
+		}
+
+		Etal trouverVendeur(Gaulois gaulois) {
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].getVendeur() == gaulois) {
+					return etals[i];
+				}
+			}
+			return null;
+		}
+		
+		
+
 	}
 }
